0a6fa10086c4a4b04be88e71c3ddd1c7ebfc96a7
Ideliya Safiullina <ideliya3004@gmail.com>
2023-11-19 23:31:32 +0300

    feat: implement register

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -52,5 +52,44 @@ namespace ReadingClubWebApp.Controllers
             TempData["Error"] = "Wrong credentials. Please try again";
             return View(loginVM);
         }
+
+        public IActionResult Register()
+        {
+            var response = new RegisterViewModel();
+            return View(response);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Register(RegisterViewModel registerVM)
+        {
+            if (!ModelState.IsValid) return View(registerVM);
+
+            var user = await _userManager.FindByEmailAsync(registerVM.EmailAddress);
+            if (user != null)
+            {
+                TempData["Error"] = "This email address is already in use";
+                return View(registerVM);
+            }
+
+            var newUser = new AppUser()
+            {
+                Email = registerVM.EmailAddress,
+                UserName = registerVM.EmailAddress
+            };
+            var newUserResponse = await _userManager.CreateAsync(newUser, registerVM.Password);
+
+            if (newUserResponse.Succeeded)
+                await _userManager.AddToRoleAsync(newUser, UserRoles.User);
+
+            return RedirectToAction("Index", "Event");
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Logout()
+        {
+            await _signInManager.SignOutAsync();
+            return RedirectToAction("Index", "Event");
+        }
+
     }
 }
--- Models/AppUser.cs
+++ Models/AppUser.cs
@@ -11,7 +11,7 @@ namespace ReadingClubWebApp.Models
         public string? PreferredGenre { get; set; }
         public string? FavoriteBook { get; set; }
         [ForeignKey("Address")]
-        public int AddressId {  get; set; }
+        public int? AddressId {  get; set; }
         public Address? Address { get; set; }
         public ICollection<Club>? Clubs { get; set; }
         public ICollection<Event>? Events { get; set; }
