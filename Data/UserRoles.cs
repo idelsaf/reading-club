16ac9149f65308e0c145a03495b4bbd86e6c484b
Ideliya Safiullina <ideliya3004@gmail.com>
2023-11-19 22:52:45 +0300

    feat: implement login

---
+++ Data/UserRoles.cs
@@ -0,0 +1,8 @@
+﻿namespace ReadingClubWebApp.Data
+{
+    public static class UserRoles
+    {
+        public const string Admin = "admin";
+        public const string User = "user";
+    }
+}
--- Models/AppUser.cs
+++ Models/AppUser.cs
@@ -1,14 +1,17 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace ReadingClubWebApp.Models
 {
-    public class AppUser
+    public class AppUser : IdentityUser
     {
-        [Key]
-        public string? Id { get; set; }
+
         public int? BooksNumber { get; set; }
         public string? PreferredGenre { get; set; }
         public string? FavoriteBook { get; set; }
+        [ForeignKey("Address")]
+        public int AddressId {  get; set; }
         public Address? Address { get; set; }
         public ICollection<Club>? Clubs { get; set; }
         public ICollection<Event>? Events { get; set; }
